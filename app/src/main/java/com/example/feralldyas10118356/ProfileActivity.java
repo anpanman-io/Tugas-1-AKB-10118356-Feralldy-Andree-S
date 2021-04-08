e9d6b01f6cfb9d79e4755de52368f18eac6d3ff6
anpanman-io <mariomuharjani@gmail.com>
2021-04-08 21:48:18 +0700

    first commit

---
+++ app/src/main/java/com/example/feralldyas10118356/ProfileActivity.java
@@ -0,0 +1,17 @@
+package com.example.feralldyas10118356;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        getSupportActionBar().hide();
+    }
+}
+
+//07-04-2021, 10118356, Feralldy Andree Sapoetra, IF9
